1b00fe08437eb6b301860c7bf57f72fe40406343
sortizaccess <sortiz@accessinformatica.com.ar>
2025-04-20 18:05:56 -0300

    Update ItemListContainer.jsx

--- ECommerce/src/components/ItemListContainer/ItemListContainer.jsx
+++ ECommerce/src/components/ItemListContainer/ItemListContainer.jsx
@@ -27,31 +27,7 @@ const ItemListContainer = () => {
       description: 'Descripción 3',
       price: '100',
       stock: 3
-    },
-    {
-      id: 4,
-      image: '',
-      name: 'Producto 1',
-      description: 'Descripción 1',
-      price: '100',
-      stock: 1
-    },
-    {
-      id: 5,
-      image: '',
-      name: 'Producto 2',
-      description: 'Descripción 2',
-      price: '100',
-      stock: 2
-    },
-    {
-      id: 6,
-      image: '',
-      name: 'Producto 3',
-      description: 'Descripción 3',
-      price: '100',
-      stock: 3
-    }    
+    }
   ]
 
 
